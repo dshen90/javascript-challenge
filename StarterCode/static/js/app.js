e7f8a5f67a246c9f99dc024facb5ac2cafe4063a
Daniel <dshen90@gmail.com>
2020-01-21 18:17:20 -0800

    added data into a table in the web page

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,13 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+data.forEach((tableData) => {
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
